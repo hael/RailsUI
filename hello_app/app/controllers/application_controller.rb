db30b13797cbd44e4808c9c7f8470f70ccc412a8
hael <hans.elmlund@nih.gov>
2025-08-16 15:40:20 -0400

    updated hello app

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+  # say hello world
+  def hello
+    render html: "hello, world!"
+  end
 end
